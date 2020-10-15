20c64f5c75d7209b8f02bd86a5967254f83a3dc0
adityak-bit <adityajsr1998@gmail.com>
2020-10-15 20:10:16 +0530

    [ADITYA] Added : Ability to add Hotels in Hotel Reservation System with name and rates for regular customer

--- main/java/com/cg/HotelReservation.java
+++ main/java/com/cg/HotelReservation.java
@@ -1,8 +1,20 @@
 package com.cg;
 
+import java.util.*;
+
 public class HotelReservation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation System");
+		Hotels Lakewood = new Hotels("Lakewood",110);
+		Hotels Bridgewood = new Hotels("Bridgewood",160);
+		Hotels Ridgewood = new Hotels("Ridgewood",220);
+		
+		List<Hotels> hotelList = new ArrayList<>();
+		
+		hotelList.add(Lakewood);
+		hotelList.add(Bridgewood);
+		hotelList.add(Ridgewood);
+		System.out.println(hotelList);
 	}
 }
---
+++ main/java/com/cg/Hotels.java
@@ -0,0 +1,12 @@
+package com.cg;
+
+public class Hotels {
+
+	private String hotelName;
+	private int weekdayRatesForRegulars;
+	
+	public Hotels(String hotelName, int weekdayRatesForRegulars) {
+		this.hotelName = hotelName;
+		this.weekdayRatesForRegulars = weekdayRatesForRegulars;
+	}
+}
