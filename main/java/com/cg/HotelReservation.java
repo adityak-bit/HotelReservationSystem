3777e55d28f973b871f75e7a48b482abaa754e71
adityak-bit <adityajsr1998@gmail.com>
2020-10-16 07:56:49 +0530

    [ADITYA] Added : Ability to find the cheapest hotel on a given date range based on weekday and weekend

--- main/java/com/cg/HotelReservation.java
+++ main/java/com/cg/HotelReservation.java
@@ -1,6 +1,7 @@
 package com.cg;
 
 import java.text.ParseException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -14,15 +15,45 @@ public class HotelReservation {
 		System.out.println("Welcome to Hotel Reservation System");
 
 		Hotels Lakewood = new Hotels("Lakewood", 110, 90);
-		Hotels Bridgewood = new Hotels("Bridgewood", 160, 50);
+		Hotels Bridgewood = new Hotels("Bridgewood", 150, 50);
 		Hotels Ridgewood = new Hotels("Ridgewood", 220, 150);
 
 		List<Hotels> hotelList = new ArrayList<>();
-		
+		TreeMap<Integer, String> totalPrice = new TreeMap<>();
+
 		hotelList.add(Lakewood);
 		hotelList.add(Bridgewood);
 		hotelList.add(Ridgewood);
 
-		System.out.println("Weekend and Weekday rates for Regular Customers for each Hotel added");
+		in = new Scanner(System.in);
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		System.out.println("Enter checkInDate as ddMMyyyy: ");
+		String checkInDate = in.nextLine();
+		System.out.println("Enter checkOutDate as ddMMyyyy: ");
+		String checkOutDate = in.nextLine();
+
+		LocalDate date1 = LocalDate.parse(checkInDate, dtf);
+		LocalDate date2 = LocalDate.parse(checkOutDate, dtf);
+		int difference = (int) ChronoUnit.DAYS.between(date1, date2);
+		int totalDays = difference + 1;
+		System.out.println("TotalDays: " + totalDays);
+
+		for (Hotels h : hotelList) {
+			int price = 0;
+
+			for (int i = 0; i < totalDays; i++) {
+				LocalDate dateIn = LocalDate.parse(checkInDate, dtf);
+				LocalDate date = dateIn.plusDays(i);
+				DayOfWeek day = date.getDayOfWeek();
+				if (day == DayOfWeek.SUNDAY || day == DayOfWeek.SATURDAY) {
+					price = price + h.weekendRatesForRegulars;
+				} else {
+					price = price + h.weekdayRatesForRegulars;
+				}
+			}
+			totalPrice.put(price, h.hotelName);
+		}
+
+		System.out.println("Cheapest hotel: " + totalPrice.firstEntry());
 	}
 }
