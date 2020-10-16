ffeb2c3d32ce76103ce496d8c6c562e8b09902aa
adityak-bit <adityajsr1998@gmail.com>
2020-10-16 08:19:28 +0530

    [ADITYA] Added : Ability to find the cheapest best rated hotel for a given date range

--- main/java/com/cg/HotelReservation.java
+++ main/java/com/cg/HotelReservation.java
@@ -19,11 +19,41 @@ public class HotelReservation {
 		Hotels Ridgewood = new Hotels("Ridgewood", 220, 150, 5);
 
 		List<Hotels> hotelList = new ArrayList<>();
+		TreeMap<Integer, Integer> totalPrice = new TreeMap<>();
 
 		hotelList.add(Lakewood);
 		hotelList.add(Bridgewood);
 		hotelList.add(Ridgewood);
 
-		System.out.println("Hotel Ratings added successfully");
+		in = new Scanner(System.in);
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		System.out.println("Enter checkInDate as ddMMyyyy: ");
+		String checkInDate = in.nextLine();
+		System.out.println("Enter checkOutDate as ddMMyyyy: ");
+		String checkOutDate = in.nextLine();
+
+		LocalDate date1 = LocalDate.parse(checkInDate, dtf);
+		LocalDate date2 = LocalDate.parse(checkOutDate, dtf);
+		int difference = (int) ChronoUnit.DAYS.between(date1, date2);
+		int totalDays = difference + 1;
+		System.out.println("TotalDays: " + totalDays);
+
+		for (Hotels h : hotelList) {
+			int price = 0;
+
+			for (int i = 0; i < totalDays; i++) {
+				LocalDate dateIn = LocalDate.parse(checkInDate, dtf);
+				LocalDate date = dateIn.plusDays(i);
+				DayOfWeek day = date.getDayOfWeek();
+				if (day == DayOfWeek.SUNDAY || day == DayOfWeek.SATURDAY) {
+					price = price + h.weekendRatesForRegulars;
+				} else {
+					price = price + h.weekdayRatesForRegulars;
+				}
+			}
+			totalPrice.put(price, h.ratings);
+		}
+
+		System.out.println("Cheapest hotel: " + totalPrice.firstEntry());
 	}
 }
